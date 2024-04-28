1793e01e024f0ac9988586420e0e005c4d810f32
jstache3 <o503@interia.eu>
2024-04-28 21:57:43 +0200

    Changed LexerException::getName()

--- src/lexer/errors.cpp
+++ src/lexer/errors.cpp
@@ -1,15 +1,12 @@
 #include "errors.hpp"
 
-#include <boost/core/demangle.hpp>
-
 LexerException::LexerException(const Position& position, const std::string& message)
     : std::runtime_error(" at " + std::to_string(position.line) + ':'
                          + std::to_string(position.column) + '\n' + message),
       position_(position) {}
 
 std::string LexerException::getName() const {
-    auto name = typeid(*this).name();
-    return boost::core::demangle(name);
+    return typeid(*this).name();
 }
 
 std::string LexerException::describe() const {
