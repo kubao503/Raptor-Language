06adfc9fbe6530018b15c4390cbd35460b15a586
jstache3 <o503@interia.eu>
2024-05-04 13:34:24 +0200

    parser.tpp file with template methods

--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -79,41 +79,6 @@ class Parser {
     Token currentToken_;
 };
 
-template <typename Exception>
-void Parser::expect(Token::Type expected, const Exception& exception) {
-    if (currentToken_.getType() != expected)
-        throw exception;
-
-    consumeToken();
-}
-
-template <typename T, typename Exception>
-T Parser::expectAndReturnValue(Token::Type expected, const Exception& exception) {
-    const auto value = currentToken_.getValue();
-    expect(expected, exception);
-    return std::get<T>(value);
-}
-
-/// LIST = [ ELEM { ',' ELEM } ]
-template <typename T, typename ElementParser>
-std::vector<T> Parser::parseList(ElementParser elementParser) {
-    std::vector<T> elements;
-
-    auto element = (this->*elementParser)();
-    if (!element)
-        return elements;
-
-    elements.push_back(std::move(*element));
-
-    while (currentToken_.getType() == Token::Type::CMA) {
-        consumeToken();
-        element = (this->*elementParser)();
-        if (!element)
-            throw SyntaxException(currentToken_.getPosition(),
-                                  "Expected element after comma");
-        elements.push_back(std::move(*element));
-    }
-    return elements;
-}
+#include "parser.tpp"
 
 #endif
---
+++ src/parser/parser.tpp
@@ -0,0 +1,43 @@
+#ifndef PARSER_TPP
+#define PARSER_TPP
+
+#include "parser.hpp"
+
+template <typename Exception>
+void Parser::expect(Token::Type expected, const Exception& exception) {
+    if (currentToken_.getType() != expected)
+        throw exception;
+
+    consumeToken();
+}
+
+template <typename T, typename Exception>
+T Parser::expectAndReturnValue(Token::Type expected, const Exception& exception) {
+    const auto value = currentToken_.getValue();
+    expect(expected, exception);
+    return std::get<T>(value);
+}
+
+/// LIST = [ ELEM { ',' ELEM } ]
+template <typename T, typename ElementParser>
+std::vector<T> Parser::parseList(ElementParser elementParser) {
+    std::vector<T> elements;
+
+    auto element = (this->*elementParser)();
+    if (!element)
+        return elements;
+
+    elements.push_back(std::move(*element));
+
+    while (currentToken_.getType() == Token::Type::CMA) {
+        consumeToken();
+        element = (this->*elementParser)();
+        if (!element)
+            throw SyntaxException(currentToken_.getPosition(),
+                                  "Expected element after comma");
+        elements.push_back(std::move(*element));
+    }
+    return elements;
+}
+
+#endif
