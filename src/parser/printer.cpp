d1d6389f57d32ea25257a23f363b960492fc5030
jstache3 <o503@interia.eu>
2024-05-06 14:42:43 +0200

    Printing binary expressions

--- src/parser/printer.cpp
+++ src/parser/printer.cpp
@@ -7,49 +7,107 @@ std::ostream& operator<<(std::ostream& stream, const Program& program) {
     return stream;
 }
 
+ExpressionPrinter BasePrinter::getSubExprPrinter() const {
+    return ExpressionPrinter(indent_ + indentWidth_);
+}
+
+StatementPrinter BasePrinter::getSubStmtPrinter() const {
+    return StatementPrinter(indent_ + indentWidth_);
+}
+
+std::string ExpressionPrinter::printBinaryExpression(const auto& expression) const {
+    return std::visit(getSubExprPrinter(), expression->lhs) + '\n'
+           + std::visit(getSubExprPrinter(), expression->rhs);
+}
+
 std::string ExpressionPrinter::operator()(
     const std::unique_ptr<DisjunctionExpression>& disjunction) const {
-    return getPrefix() + "DisjunctionExpression\n"
-           + std::visit(ExpressionPrinter(indent_ + indentWidth_), disjunction->lhs)
-           + '\n'
-           + std::visit(ExpressionPrinter(indent_ + indentWidth_), disjunction->rhs);
+    return getPrefix() + "DisjunctionExpression\n" + printBinaryExpression(disjunction);
 }
 
 std::string ExpressionPrinter::operator()(
     const std::unique_ptr<ConjunctionExpression>& conjunction) const {
-    return getPrefix() + "ConjunctionExpression\n"
-           + std::visit(ExpressionPrinter(indent_ + indentWidth_), conjunction->lhs)
-           + '\n'
-           + std::visit(ExpressionPrinter(indent_ + indentWidth_), conjunction->rhs);
+    return getPrefix() + "ConjunctionExpression\n" + printBinaryExpression(conjunction);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<EqualExpression>& expr) const {
+    return getPrefix() + "EqualExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<NotEqualExpression>& expr) const {
+    return getPrefix() + "NotEqualExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<LessThanExpression>& expr) const {
+    return getPrefix() + "LessThanExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<LessThanOrEqualExpression>& expr) const {
+    return getPrefix() + "LessThanOrEqualExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<GreaterThanExpression>& expr) const {
+    return getPrefix() + "GreaterThanExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<GreaterThanOrEqualExpression>& expr) const {
+    return getPrefix() + "GreaterThanOrEqualExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<AdditionExpression>& expr) const {
+    return getPrefix() + "AdditionExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<SubtractionExpression>& expr) const {
+    return getPrefix() + "SubtractionExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<MultiplicationExpression>& expr) const {
+    return getPrefix() + "MultiplicationExpression\n" + printBinaryExpression(expr);
+}
+
+std::string ExpressionPrinter::operator()(
+    const std::unique_ptr<DivisionExpression>& expr) const {
+    return getPrefix() + "DivisionExpression\n" + printBinaryExpression(expr);
 }
 
 std::string ExpressionPrinter::operator()(const auto& expr) const {
     return getPrefix() + typeid(expr).name();
 }
 
+std::string StatementPrinter::printIfOrWhileStatement(
+    const IfOrWhileStatement& ifOrWhile) const {
+    std::string output = getPrefix() + "└condition:\n"
+                         + std::visit(getSubExprPrinter(), ifOrWhile.condition) + '\n'
+                         + getPrefix() + "└statements {";
+    for (const auto& stmt : ifOrWhile.statements)
+        output += '\n' + std::visit(getSubStmtPrinter(), stmt);
+    return output + '\n' + getPrefix() + '}';
+}
+
 std::string StatementPrinter::operator()(const IfStatement& ifStatement) const {
-    std::string output = getPrefix() + "IfStatement ";
-    for (const auto& stmt : ifStatement.statements)
-        output += '\n' + std::visit(StatementPrinter(indent_ + indentWidth_), stmt);
-    return output;
+    return getPrefix() + "IfStatement\n" + printIfOrWhileStatement(ifStatement);
 }
 
 std::string StatementPrinter::operator()(const WhileStatement& whileStatement) const {
-    std::string output =
-        getPrefix() + "WhileStatement\n" + getPrefix() + "└condition:\n"
-        + std::visit(ExpressionPrinter(indent_ + indentWidth_), whileStatement.condition)
-        + '\n' + getPrefix() + "└statements {";
-    for (const auto& stmt : whileStatement.statements)
-        output += '\n' + std::visit(StatementPrinter(indent_ + indentWidth_), stmt);
-    return output + '\n' + getPrefix() + '}';
+    return getPrefix() + "WhileStatement\n" + printIfOrWhileStatement(whileStatement);
 }
 
 std::string StatementPrinter::operator()(const FuncDef& funcDef) const {
     std::string output = getPrefix() + "FuncDef " + funcDef.getName();
     for (const auto& param : funcDef.getParameters()) output += ' ' + param.name + ',';
-    output += " }";
+    output += " {";
     for (const auto& stmt : funcDef.getStatements())
-        output += '\n' + std::visit(StatementPrinter(indent_ + indentWidth_), stmt);
+        output += '\n' + std::visit(getSubStmtPrinter(), stmt);
     return output + '\n' + getPrefix() + '}';
 }
 
--- src/parser/printer.hpp
+++ src/parser/printer.hpp
@@ -7,6 +7,9 @@
 
 using namespace std::string_literals;
 
+class ExpressionPrinter;
+class StatementPrinter;
+
 class BasePrinter {
    public:
     BasePrinter(unsigned indent = 0)
@@ -14,6 +17,9 @@ class BasePrinter {
 
    protected:
     std::string getPrefix() const { return std::string(indent_, ' '); }
+
+    ExpressionPrinter getSubExprPrinter() const;
+    StatementPrinter getSubStmtPrinter() const;
 
     unsigned indent_{0};
     static constexpr unsigned indentWidth_{4};
@@ -27,7 +33,21 @@ class ExpressionPrinter : public BasePrinter {
         const std::unique_ptr<DisjunctionExpression>& disjunction) const;
     std::string operator()(
         const std::unique_ptr<ConjunctionExpression>& conjunction) const;
+    std::string operator()(const std::unique_ptr<EqualExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<NotEqualExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<LessThanExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<LessThanOrEqualExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<GreaterThanExpression>& expr) const;
+    std::string operator()(
+        const std::unique_ptr<GreaterThanOrEqualExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<AdditionExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<SubtractionExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<MultiplicationExpression>& expr) const;
+    std::string operator()(const std::unique_ptr<DivisionExpression>& expr) const;
     std::string operator()(const auto& expr) const;
+
+   private:
+    std::string printBinaryExpression(const auto& expression) const;
 };
 
 class StatementPrinter : public BasePrinter {
@@ -39,6 +59,9 @@ class StatementPrinter : public BasePrinter {
     std::string operator()(const FuncDef& funcDef) const;
     std::string operator()(const Assignment&) const;
     std::string operator()(const auto& stmt) const;
+
+   private:
+    std::string printIfOrWhileStatement(const IfOrWhileStatement& ifOrWhile) const;
 };
 
 std::ostream& operator<<(std::ostream& stream, const Program& program);
